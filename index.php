6ef475beac9a2ba8c52fd70d7c81d48c5c6b0deb
eemilyy <emilyalmeeidas@gmail.com>
2021-10-31 20:29:55 -0300

    creating the inputs in HTML

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Calculator</title>
+</head>
+<body>
+    <form action="" class="form">
+        <h1 class="title">Calculator in PHP</h1>
+        <input type="text" name="submit1" class="send-box" placeholder="First Number">
+        <input type="text" name="operator" class="send-box op" placeholder="Operator">
+        <input type="text" name="submit2" class="send-box" placeholder="Second Number"><br>
+        <input type="button" value="Done!" class="btn">
+        <input type="text" name="result" class="send-box hidden">
+    </form>
+</body>
+</html>
