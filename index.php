fa16c90f750c7a5efe1dcf585b672541d073d6cc
eemilyy <emilyalmeeidas@gmail.com>
2021-10-31 20:37:16 -0300

    add the css

--- index.php
+++ index.php
@@ -8,13 +8,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Simple Calculator</title>
 </head>
 <body>
     <form action="" class="form">
         <h1 class="title">Calculator in PHP</h1>
         <input type="text" name="submit1" class="send-box" placeholder="First Number">
-        <input type="text" name="operator" class="send-box op" placeholder="Operator">
+        <input type="text" name="operator" class="send-box" placeholder="Operator">
         <input type="text" name="submit2" class="send-box" placeholder="Second Number"><br>
         <input type="button" value="Done!" class="btn">
         <input type="text" name="result" class="send-box hidden">
